21686ac6b8713363f04be1205d915381eaf3b939
Régis Iozzino <iozzino.r@gmail.com>
2019-03-15 15:56:54 +0100

    Separated SCT browsing.

--- Script_odont/Graphics/SctBrowsing/SctBrowsingViewController.swift
+++ Script_odont/Graphics/SctBrowsing/SctBrowsingViewController.swift
@@ -10,21 +10,122 @@ import UIKit
 
 class SctBrowsingViewController: UIViewController
 {
+    // -------------------------------------------------------------------------
+    // MARK: - BROWSING SECTION
+    // -------------------------------------------------------------------------
+    fileprivate enum BrowsingSection
+    {
+        /*
+         10 most recent sct
+         date (year, month, week) + count
+        */
+        case new
+        
+        /*
+         10 best rated scts
+         see all
+        */
+        case top
+        
+        /*
+         10 personnalised scts
+         see + count
+        */
+        case personnalized
+        
+        // topic + count
+        case topics
+        
+        case search
+        
+        var headerTitle: String? {
+            switch self
+            {
+            case .new:
+                return "ScrBrowsing.Section.New.Title".localized
+            case .top:
+                return "ScrBrowsing.Section.Top.Title".localized
+            case .personnalized:
+                return "ScrBrowsing.Section.Personnalized.Title".localized
+            case .topics:
+                return "ScrBrowsing.Section.Topics.Title".localized
+            case .search:
+                return "ScrBrowsing.Section.Search.Title".localized
+            }
+        }
+        
+        var rows: [BrowsingRow] {
+            switch self
+            {
+            case .new:
+                return Array<BrowsingRow>(repeating: .newSct(Sct()), count: 10)
+            case .top:
+                return Array<BrowsingRow>(repeating: .top(Sct()), count: 10)
+            case .personnalized:
+                return []
+            case .topics:
+                return QualificationTopic.allCases.map { .topic($0) }
+            case .search:
+                return [.search]
+            }
+        }
+    }
+    
+    // -------------------------------------------------------------------------
+    // MARK: - BROWSING ROW
+    // -------------------------------------------------------------------------
+    fileprivate enum BrowsingRow
+    {
+        case newSct(Sct)
+        case newDate()
+        case top(Sct)
+        
+        case topic(QualificationTopic)
+        
+        case search
+        
+        func cell(for indexPath: IndexPath, tableView: UITableView, sctBrowsingViewController: SctBrowsingViewController) -> UITableViewCell
+        {
+            let cell = UITableViewCell()
+            cell.accessoryType = .none
+            
+            switch self
+            {
+            case let .newSct(sct):
+                cell.textLabel?.text = "New SCT \(sct.questions.count)"
+            case .search:
+                cell.textLabel?.text = "Want more ? Click here !"
+                cell.accessoryType = .disclosureIndicator
+            case let .topic(topic):
+                
+                let result = UITableViewCell(style: .value1, reuseIdentifier: nil)
+                result.textLabel?.text = topic.name
+                result.detailTextLabel?.text = "\(Constants.random(min: 10, max: 20))"
+                result.selectionStyle = .none
+                
+                result.accessoryType = .disclosureIndicator
+                return result
+            default:
+                break
+            }
+            return cell
+        }
+    }
+    
     static let scaHorizontalSegueId = "MainToSctHorizontalSegueId"
     static let walkthroughSegueId = "MainToWalkthroughSegueId"
     
-    fileprivate var checkFirstTime_ = false
+    @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var rateStar: RateStar!
-    @IBOutlet weak var partialRateStar: PartialRateStar!
-    @IBOutlet weak var slider: UISlider!
+    fileprivate var checkFirstTime_ = false
+    fileprivate let sections_: [BrowsingSection] = [.new, .top, .topics, .personnalized, .search]
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        rateStar.isSelected = true
-        slider.addTarget(self, action: #selector(SctBrowsingViewController.sliderChanged_), for: .valueChanged)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool)
@@ -39,14 +140,6 @@ class SctBrowsingViewController: UIViewController
                  self.launchWalkthrough_()
             })
         }
-    }
-    
-    // -------------------------------------------------------------------------
-    // MARK: - ACTIONS
-    // -------------------------------------------------------------------------
-    @objc fileprivate func sliderChanged_()
-    {
-        partialRateStar.value = Double(slider.value)
     }
     
     // -------------------------------------------------------------------------
@@ -105,3 +198,41 @@ class SctBrowsingViewController: UIViewController
     }
 }
 
+// -----------------------------------------------------------------------------
+// MARK: - UITableViewDelegate
+// -----------------------------------------------------------------------------
+extension SctBrowsingViewController: UITableViewDelegate
+{
+    
+}
+
+// -----------------------------------------------------------------------------
+// MARK: - UITableViewDataSource
+// -----------------------------------------------------------------------------
+extension SctBrowsingViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        let currentSection = sections_[section]
+        return currentSection.headerTitle
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return sections_.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        let currentSection = sections_[section]
+        return currentSection.rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let section = sections_[indexPath.section]
+        let row = section.rows[indexPath.row]
+        
+        return row.cell(for: indexPath, tableView: tableView, sctBrowsingViewController: self)
+    }
+}
---
+++ Script_odont/Graphics/SctBrowsing/SctSearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SctSearchViewController.swift
+//  Script_odont
+//
+//  Created by Régis Iozzino on 15/03/2019.
+//  Copyright © 2019 Régis Iozzino. All rights reserved.
+//
+
+import UIKit
+
+class SctSearchViewController: UIViewController
+{
+    
+}
