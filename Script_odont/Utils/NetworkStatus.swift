d87a35273db00c799f422533ff862d32ed298372
Régis Iozzino <iozzino.r@gmail.com>
2019-03-20 08:39:34 +0100

    Added network status recognition.

---
+++ Script_odont/Utils/NetworkStatus.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkStatus.swift
+//  Script_odont
+//
+//  Created by Régis Iozzino on 20/03/2019.
+//  Copyright © 2019 Régis Iozzino. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkStatus
+{
+    case notReachable
+    case reachableViaWifi
+    case reachableViaWwan
+}
---
+++ Script_odont/Utils/Reachability.swift
@@ -0,0 +1,58 @@
+//
+//  Reachability.swift
+//  Script_odont
+//
+//  Created by Régis Iozzino on 20/03/2019.
+//  Copyright © 2019 Régis Iozzino. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+struct Reachability
+{
+    fileprivate static let googleDnsHostName = "8.8.8.8"
+    fileprivate static let reachability_ = SCNetworkReachabilityCreateWithName(nil, googleDnsHostName)
+    
+    fileprivate static func networkStatus_(for flags: SCNetworkReachabilityFlags) -> NetworkStatus
+    {
+        if !flags.contains(.reachable)
+        {
+            return .notReachable
+        }
+        
+        var result = NetworkStatus.notReachable
+        
+        if !flags.contains(.connectionRequired)
+        {
+            // the flags does not contain the required connection flag
+            // assume the Wifi is enabled
+            result = .reachableViaWifi
+        }
+        
+        if flags.contains(.isWWAN)
+        {
+            result = .reachableViaWwan
+        }
+        
+        return result
+    }
+    
+    static var currentStatus: NetworkStatus
+    {
+        guard let reachability = Reachability.reachability_ else
+        {
+            return .notReachable
+        }
+        
+        var result = NetworkStatus.notReachable
+        
+        var flags = SCNetworkReachabilityFlags()
+        if SCNetworkReachabilityGetFlags(reachability, &flags)
+        {
+            result = Reachability.networkStatus_(for: flags)
+        }
+        
+        return result
+    }
+}
