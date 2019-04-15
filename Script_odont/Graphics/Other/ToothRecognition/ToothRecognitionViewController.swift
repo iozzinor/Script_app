c06db821eb3a425161c12202e3bec097c5db4b30
Régis Iozzino <iozzino.r@gmail.com>
2019-04-15 09:03:24 +0200

    Load tooth to recognize asynchronously.

--- Script_odont/Graphics/Other/ToothRecognition/ToothRecognitionViewController.swift
+++ Script_odont/Graphics/Other/ToothRecognition/ToothRecognitionViewController.swift
@@ -12,13 +12,17 @@ import ToothCommon
 
 class ToothRecognitionViewController: UIViewController
 {
-    static let toToothRecognitionSelection = "ToothRecognitionToToothRecognitionSelectionSegueId"
+    static let toToothRecognitionSelection  = "ToothRecognitionToToothRecognitionSelectionSegueId"
+    static let toVolume                     = "ToothRecognitionToVolumeSegueId"
     
     @IBOutlet weak var monitoringLabel: UILabel!
-    @IBOutlet weak var sceneView: SCNView!
+    @IBOutlet weak var toothView: SCNView!
     @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     fileprivate var toothToRecognize_ = Tooth(internationalNumber: 11)
+    fileprivate var attempts_ = 0
+    fileprivate var successes_ = 0
     
     // -------------------------------------------------------------------------
     // MARK: - VIEW CYCLE
@@ -27,7 +31,6 @@ class ToothRecognitionViewController: UIViewController
     {
         super.viewDidLoad()
         
-        toothToRecognize_ = randomTooth_()
         setup_()
     }
     
@@ -44,7 +47,11 @@ class ToothRecognitionViewController: UIViewController
     fileprivate func setup_()
     {
         setupMonitoringLabel_()
+        setupToothScene_()
         setupSelectButton_()
+        setupActivityIndicator_()
+        
+        pickTooth_()
     }
     
     fileprivate func setupMonitoringLabel_()
@@ -52,9 +59,46 @@ class ToothRecognitionViewController: UIViewController
         monitoringLabel.text = "ToothRecognition.FirstQuestion".localized
     }
     
+    fileprivate func setupToothScene_()
+    {
+        // tooth view
+        toothView.autoenablesDefaultLighting = true
+        toothView.allowsCameraControl = true
+        toothView.defaultCameraController.interactionMode = .orbitAngleMapping
+        
+        // scene
+        let scene = SCNScene()
+        scene.background.contents = UIColor.black
+        toothView.scene = scene
+        
+        // gesture recognizer
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        tapGestureRecognizer.addTarget(self, action: #selector(ToothRecognitionViewController.toothViewTouched_))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        tapGestureRecognizer.numberOfTouchesRequired = 1
+        toothView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
     fileprivate func setupSelectButton_()
     {
         selectButton.setTitle("ToothRecognition.Select".localized, for: .normal)
+    }
+    
+    fileprivate func setupActivityIndicator_()
+    {
+        activityIndicator.color = UIColor.white
+        
+        view.bringSubviewToFront(activityIndicator)
+        
+        activityIndicator.isHidden = true
+    }
+    
+    // -------------------------------------------------------------------------
+    // MARK: - UPDATE
+    // -------------------------------------------------------------------------
+    fileprivate func updateMonitoringLabel_()
+    {
+        monitoringLabel.text = String.localizedStringWithFormat("ToothRecognition.Monitoring".localized, successes_, attempts_)
     }
     
     // -------------------------------------------------------------------------
@@ -69,10 +113,63 @@ class ToothRecognitionViewController: UIViewController
         return result
     }
     
+    fileprivate func pickTooth_()
+    {
+        toothToRecognize_ = randomTooth_()
+        
+        let fileUrl = pickToothFile_()
+        loadNode_(forUrl: fileUrl, completion: {
+            (node, error) -> Void in
+            
+            DispatchQueue.main.async {
+                if let node = node
+                {
+                    self.displayNode_(node)
+                }
+            }
+        })
+    }
+    
     fileprivate func pickToothFile_() -> URL
     {
-        return URL(string: "")!
-        //Bundle.main.urls(forResourcesWithExtension: <#T##String?#>, subdirectory: <#T##String?#>)
+        return Bundle.main.url(forResource: "antagonist", withExtension: "stl", subdirectory: "SctData")!
+    }
+    
+    fileprivate func loadNode_(forUrl url: URL, completion: ((SCNNode?, Error?) -> Void)?)
+    {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        DispatchQueue.global(qos: .utility).async {
+            do
+            {
+                let node = try SCNNode.load(stlFileUrl: url)
+                completion?(node, nil)
+            }
+            catch
+            {
+               completion?(nil, error)
+            }
+        }
+    }
+    
+    fileprivate func displayNode_(_ node: SCNNode)
+    {
+        // remove nodes
+        if let childNodes = toothView.scene?.rootNode.childNodes
+        {
+            for childNode in childNodes
+            {
+                childNode.removeFromParentNode()
+            }
+        }
+        
+        // add the new one
+        toothView.scene?.rootNode.addChildNode(node)
+        
+        // stop the activity indicator
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
     }
     
     // -------------------------------------------------------------------------
@@ -95,6 +192,11 @@ class ToothRecognitionViewController: UIViewController
     {
         performSegue(withIdentifier: ToothRecognitionViewController.toToothRecognitionSelection, sender: self)
     }
+    
+    @objc fileprivate func toothViewTouched_(_ sender: UITapGestureRecognizer)
+    {
+        performSegue(withIdentifier: ToothRecognitionViewController.toVolume, sender: self)
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -108,6 +210,13 @@ extension ToothRecognitionViewController: ToothRecognitionSelectionDelegate
     
     func toothRecognitionSelection(_ toothRecognitionSelection: ToothRecognitionSelectionViewController, didSelect tooth: Tooth, correctTooth: Tooth)
     {
+        attempts_ += 1
+        if tooth == correctTooth
+        {
+            successes_ += 1
+        }
+        updateMonitoringLabel_()
+        
         toothToRecognize_ = randomTooth_()
     }
 }
---
+++ Script_odont/Graphics/Volume/VolumeViewController.swift
@@ -0,0 +1,131 @@
+//
+//  VolumeViewController.swift
+//  Script_odont
+//
+//  Created by Régis Iozzino on 29/03/2019.
+//  Copyright © 2019 Régis Iozzino. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class VolumeViewController: UIViewController
+{
+    @IBOutlet weak var toggleMeshButton: UIBarButtonItem!
+    
+    fileprivate var displayBar_ = false
+    
+    var scene: SCNScene! = SCNScene() {
+        didSet {
+            if isViewLoaded
+            {
+                scnView.scene = scene
+            }
+        }
+    }
+    var scnView: SCNView!
+    fileprivate var displayMesh_ = true
+    fileprivate var meshNode_: SCNNode? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+        setup_()
+    }
+    
+    // -------------------------------------------------------------------------
+    // MARK: - SETUP
+    // -------------------------------------------------------------------------
+    fileprivate func setup_()
+    {
+        setupScene_()
+        setupGestureRecognizer_()
+    }
+    
+    fileprivate func setupScene_()
+    {
+        if let sceneView = view as? SCNView
+        {
+            self.scnView = sceneView
+        }
+        scnView.scene = scene
+        
+        scnView.autoenablesDefaultLighting = true
+        scnView.allowsCameraControl = true
+        scnView.showsStatistics = true
+        scnView.defaultCameraController.interactionMode = .orbitAngleMapping
+        
+        if scene.rootNode.childNodes.count > 1
+        {
+            meshNode_ = scene.rootNode.childNodes[1]
+        }
+        
+        updateTooggleMeshButtonTitle_(shouldDisplayMesh: true)
+    }
+    
+    fileprivate func setupGestureRecognizer_()
+    {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(VolumeViewController.viewTapped_))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTapsRequired = 1
+        scnView.addGestureRecognizer(tapGesture)
+    }
+    
+    // -------------------------------------------------------------------------
+    // MARK: - UI
+    // -------------------------------------------------------------------------
+    
+    fileprivate func updateTooggleMeshButtonTitle_(shouldDisplayMesh: Bool)
+    {
+        let meshToDisplay = (meshNode_ != nil)
+        let isDisplayingMesh = (meshToDisplay && shouldDisplayMesh)
+        
+        let title = isDisplayingMesh ? "Volume.ToggleMeshButton.Title.Hide".localized : "Volume.ToggleMeshButton.Title.Display".localized
+        toggleMeshButton.title = title
+        
+        toggleMeshButton.isEnabled = meshToDisplay
+    }
+    
+    // -------------------------------------------------------------------------
+    // MARK: - ACTIONS
+    // -------------------------------------------------------------------------
+    @IBAction func cancel(_ sender: UIBarButtonItem)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc fileprivate func viewTapped_()
+    {
+        displayBar_ = !displayBar_
+        navigationController?.navigationBar.isHidden = !displayBar_
+    }
+    
+    @IBAction func toggleMesh(_ sender: UIBarButtonItem)
+    {
+        displayMesh_ = !displayMesh_
+        updateTooggleMeshButtonTitle_(shouldDisplayMesh: displayMesh_)
+        
+        if displayMesh_,
+            let mesh = meshNode_
+        {
+            scene.rootNode.addChildNode(mesh)
+        }
+        else
+        {
+            meshNode_?.removeFromParentNode()
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// MARK: - UI VIEW CONTROLLER TRANSITIONING DELEGATE
+// -----------------------------------------------------------------------------
+extension VolumeViewController: UIViewControllerTransitioningDelegate
+{
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning?
+    {
+        return ImageFadeOutAnimationController()
+    }
+}
